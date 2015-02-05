481c6e0017c8bec90bb60eae7c152a04299e534a
Bobby Allen <ballen@bobbyallen.me>
2015-02-05 08:53:07 +0000

    Updated Factory class to instantiate the Sentior class with the defaults.

--- src/SenitorFactory.php
+++ src/SenitorFactory.php
@@ -1,19 +1,26 @@
 <?php namespace Ballen\Senitor;
 
+use Ballen\Senitor\Senitor;
+use Ballen\Senitor\Entities\Target;
+
 class SenitorFactory
 {
 
     /**
      * Create a new Senitor instance
-     * @param string $server
-     * @param string $api_key
-     * @param string $user
-     * @param string $pass
-     * @param array $http_client_options
+     * @param string $server The Sentora Server base URL (eg. https://cp.yourdomain.com/)
+     * @param string $api_key The Sentora Server API key.
+     * @param string $user A username on the server.
+     * @param string $pass A password on the server.
+     * @param array $http_client_options Optional settings for the Guzzle/cURL client.
      * @return \Ballen\Senitor\Senitor
      */
     public static function create($server, $api_key, $user, $pass, $http_client_options = null)
     {
-       
+        $sentora_client = new Senitor(
+            new Target($server, $user, $pass, $api_key)
+        );
+        $sentora_client->setHttpOptions($http_client_options);
+        return $sentora_client;
     }
 }
