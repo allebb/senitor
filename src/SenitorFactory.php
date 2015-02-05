a05a62e204f05a5bf5a5e4363dec3516286393eb
Bobby Allen <ballen@bobbyallen.me>
2015-02-05 08:47:46 +0000

    Adds new Factory class.

---
+++ src/SenitorFactory.php
@@ -0,0 +1,19 @@
+<?php namespace Ballen\Senitor;
+
+class SenitorFactory
+{
+
+    /**
+     * Create a new Senitor instance
+     * @param string $server
+     * @param string $api_key
+     * @param string $user
+     * @param string $pass
+     * @param array $http_client_options
+     * @return \Ballen\Senitor\Senitor
+     */
+    public static function create($server, $api_key, $user, $pass, $http_client_options = null)
+    {
+       
+    }
+}
